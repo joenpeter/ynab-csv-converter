9994da426867b9c369e3f5b7e111047a73e691b7
Joen Peter <joen.peter.ext@extendaretail.com>
2022-08-02 17:49:33 +0200

    feat: Bring in springboot basics
    Signed-off-by: Joen Peter <joen.peter.ext@extendaretail.com>

---
+++ src/main/java/tech/droptable/ynab/converter/client/YnabCsvConverterClientApplication.java
@@ -0,0 +1,13 @@
+package tech.droptable.ynab.converter.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YnabCsvConverterClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YnabCsvConverterClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/droptable/ynab/converter/client/YnabCsvConverterClientApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.droptable.ynab.converter.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YnabCsvConverterClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
